f9528523845d458ffe0ae5654dd8a8443f2332b8
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-13 16:36:43 -0700

    adding comment to the main function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 	/*
-	 * writing the text
+	 * main- writing the text
+	 * Return: Always 1 (Success)
 	 */
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
