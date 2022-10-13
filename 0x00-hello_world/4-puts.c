83dfc21089b60cb0d87682e7c075379d6531121b
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-13 15:01:41 -0700

    adjusting the comment and betty style rule

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/*
- *Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
- */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
