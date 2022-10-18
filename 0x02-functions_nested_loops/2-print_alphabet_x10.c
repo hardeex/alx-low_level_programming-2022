b3c1e0a02c71511c26c4335f6091e621d48c1412
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-18 16:09:47 -0700

    error correction without considering the betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * A function that prints 10 times the alphated in lower case
- */
-
+ *  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *   */
 void print_alphabet_x10(void)
 {
-	int count= 1;
-	char letter;
+		int count = 0;
+			char letter;
 
-	while(count <=10)
-	{
-		letter='a'
-			while (letter <='z')
-			{
-				_putchar(letter);
-				letter++
-	}
-		_putchar('\n')
-			count++;
-
+				while (count++ <= 9)
+				{
+					for (letter = 'a'; letter <= 'z'; letter++)
+						_putchar(letter);
+					_putchar('\n');
+				}
 }
