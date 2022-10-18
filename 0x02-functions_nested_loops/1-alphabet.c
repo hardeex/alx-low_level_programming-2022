65a3db7b49008a9d151284faa84cd7740dda93d2
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-18 09:46:46 -0700

    task on function prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /*
  * A program that print letter a to z
  */
@@ -10,6 +11,6 @@ void print_alphabet(void)
 	for(letter='a'; letter <= 'z' letter++)
 		_putchar(letter);
 
-	_putchar("\n";
+	_putchar('\n');
 
 }
