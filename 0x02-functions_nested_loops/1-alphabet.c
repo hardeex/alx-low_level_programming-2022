9329d6bcce8c663dd21fb0ecc8102eff55d20d39
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-18 15:43:19 -0700

    nested functions task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
-/*
- * A program that print letter a to z
+/**
+ *  print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
 void print_alphabet(void)
 {
-	char letter = 'a';
-		while(letter <='z)
-		{
-			_putchar(letter);
-			letter++;
-		}
+		char letter;
 
-	_putchar('\n');
+			for (letter = 'a'; letter <= 'z'; letter++)
+						_putchar(letter);
 
+				_putchar('\n');
 }
