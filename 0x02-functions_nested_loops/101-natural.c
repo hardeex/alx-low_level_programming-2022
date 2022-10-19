21942adb9c4a572621b50f43d63200a796c20c53
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-19 06:57:54 -0700

    appying betty styles to the program

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,18 +5,16 @@
  *   *        that are multiples of 3 or 5.
  *    *
  *     * Return: Always 0.
- *      */
+ */
 int main(void)
 {
 	int i, sum = 0;
-	
-		for (i = 0; i < 1024; i++)
-		{
-			if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
-		}
-		
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
 		printf("%d\n", sum);
-		
 		return (0);
 }
