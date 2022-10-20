77a7128b4d7936642e885f237172bf7cb0e70d72
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-20 03:56:12 -0700

    debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main- testing the function that prints if a number is postive or negative
+ * Return: 0 upon successful execution
+ */
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+}
