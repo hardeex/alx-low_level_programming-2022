7b64dd0a29acd12f99705f318e82b0fe6aa8a129
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2022-10-24 07:12:23 -0700

    adding README and task o funtion

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98: A funtion that takes pointer to an int as parameter
+ * and update the value it point to to 98
+ * @*n: The pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	printf("The pointer %p \n", n);
+}
+
